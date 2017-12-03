8b9804c341b061f875c845c4a7a9d87ab98e62b6
unknown <shahd5@hawkmail.newpaltz.edu>
2017-12-02 19:43:43 -0500

    POST and GET responses

--- server.js
+++ server.js
@@ -4,14 +4,32 @@ var path = require("path");
 
 http.createServer(function(req,res){
     console.log(`${req.method} request for ${req.url}`);
-    if(req.url ==="/"){
-        fs.readFile("./public/view.html","UTF-8",function(err,html){
-            res.writeHead(200,{"Content-Type":"text/html"});
-            res.end(html);
+    if(req.method ==="GET"){
+        if(req.url ==="/"){
+            fs.readFile("./public/view.html","UTF-8",function(err,html){
+                res.writeHead(200,{"Content-Type":"text/html"});
+                res.end(html);
+            });
+        }else if(req.url ==="/viewtimesheet"){
+            res.end("hello  world");
+        }else{
+            res.writeHead(404,{"Content-Type":"text/plain"});
+            res.end("404 File not found");
+        }
+    }else if(req.method ==="POST"){
+        var body = "";
+        req.on("data",function(chunk){
+            body += chunk;
         });
-    }else{
-        res.writeHead(404,{"Content-Type":"text/plain"});
-        res.end("404 File not found");
+        req.on("end",function(){
+            fs.readFile("./public/index.html","UTF-8",function(err,html){
+                res.writeHead(200,{"Content-Type":"text/html"});
+                res.end(html);
+            });
+        });
     }
+    
 }).listen(3000);
+
+console.log("server running on port 3000");
 
